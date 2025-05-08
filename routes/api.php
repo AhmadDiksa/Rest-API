bec0f4f347e5afa0a4f1aed782d108b8f02e1d42
AhmadDiksa <dionjombang300@gmail.com>
2025-05-08 21:55:07 +0700

    Add UserSeeder and update DatabaseSeeder to include UserSeeder

--- routes/api.php
+++ routes/api.php
@@ -10,9 +10,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-use App\Http\Middleware\BasicAuthUsernameMiddleware;
-
-Route::middleware(BasicAuthUsernameMiddleware::class)->apiResource('dosen', DosenController::class);
+Route::middleware('auth:api')->apiResource('dosen', DosenController::class);
 
 Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
 Route::post('/mahasiswa', [MahasiswaController::class, 'store']);
